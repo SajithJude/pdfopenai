3f5adbcd3fb48ddf5de3d5240f4afecb42dfa3f8
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-08 16:32:32 +0000

    sss

--- pages/layout.py
+++ pages/layout.py
@@ -8,5 +8,5 @@ uploaded_file = st.file_uploader("Choose a file", "pdf")
 if uploaded_file is not None:
     for page_layout in extract_pages(uploaded_file):
         st.code(page_layout, language='xmlDoc')
-        # for element in page_layout:
-        #     st.code(element)
+        for element in page_layout:
+            st.code(element, language='xmlDoc')
