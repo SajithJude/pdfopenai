179b0e671e8aa7d2d2faf3c22d0bcf0c52ad6624
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-08 19:26:10 +0000

    ddd

---
+++ pages/SQL_OpenAI.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import openai
+
+
+openai.api_key =  os.getenv("API_KEY")
+
+nat = st.text_input("Enter the query in natural language ")
+
+
+response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt="Generate an SQL query from this natural language query :" + str(nat) + "  , for a database consisting of 1200 tools related to AI",
+            temperature=0.56,
+            max_tokens=2066,
+            top_p=1,
+            frequency_penalty=0.35,
+            presence_penalty=0
+            
+            )
+st.code(response.choices[0].text)
