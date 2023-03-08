23782c609fa65cb8c876c30e28ccd18deb6206f0
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-08 16:58:13 +0000

    sss

--- pages/layout.py
+++ pages/layout.py
@@ -17,6 +17,7 @@ uploaded_file = st.file_uploader("Choose a file", "pdf")
 if uploaded_file is not None:
     txt= extract_text_to_fp(fin, output_string, laparams=LAParams(),
                         output_type='html', codec=None)
+    st.write(txt)
     st.markdown(txt,  unsafe_allow_html=True)
 
     # for page_layout in extract_pages(uploaded_file):
