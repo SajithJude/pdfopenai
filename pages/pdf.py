504fe92d88d8570d6c6ccbf23ee935124dc5773c
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-08 17:43:42 +0000

    jjj

--- pages/pdf.py
+++ pages/pdf.py
@@ -10,28 +10,6 @@ from pdfminer.layout import LAParams, LTTextBoxHorizontal, LTTextLineHorizontal
 from pdfminer.converter import TextConverter, PDFPageAggregator
 
 
-def extract_layout(pdf_path):
-    # Open the PDF file
-    # with open(pdf_path, 'rb') as fp:
-        # Create a PDF parser object
-    parser = PDFParser(pdf_path)
-    # Create a PDF document object
-    doc = PDFDocument(parser)
-    # Create a PDF resource manager object
-    rsrcmgr = PDFResourceManager()
-    # Set parameters for analysis
-    laparams = LAParams()
-    # Create a PDF device object
-    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-    # Create a PDF interpreter object
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    # Extract the layout information of each page
-    layouts = []
-    for page in doc.get_pages():
-        interpreter.process_page(page)
-        layout = device.get_result()
-        layouts.append(layout)
-    return layouts
 
 
 # Define the Streamlit app
@@ -51,5 +29,11 @@ if pdf_file is not None:
     for i, layout in enumerate(layouts):
         st.header(f"Page {i+1}")
         for element in layout:
+
             if isinstance(element, (LTTextBoxHorizontal, LTTextLineHorizontal)):
-                st.write(f"{element.get_text().strip()} ({element.x0}, {element.y0}, {element.x1}, {element.y1})")
+
+                node_level = len(element.get_text().split())
+                                   # Create an expander for the element based on its node level
+                with st.beta_expander(f"{element.get_text().strip()} ({element.x0}, {element.y0}, {element.x1}, {element.y1})", expanded=node_level <= 1):
+
+                    st.write(f"Node level: {node_level}")
