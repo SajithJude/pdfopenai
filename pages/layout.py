1cb0b45571f3f347dd56987860d17fcab2d743a0
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-08 16:33:57 +0000

    sss

--- pages/layout.py
+++ pages/layout.py
@@ -7,6 +7,6 @@ st.write(pdfminer.__version__)
 uploaded_file = st.file_uploader("Choose a file", "pdf")
 if uploaded_file is not None:
     for page_layout in extract_pages(uploaded_file):
-        st.code(page_layout, language='xmlDoc')
+        # st.code(page_layout, language='xmlDoc')
         for element in page_layout:
             st.code(element, language='xmlDoc')
