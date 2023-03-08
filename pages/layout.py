b78d73e0feb3408dd7b5898831ca2c06ed440e1b
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-08 17:00:48 +0000

    jjjj

--- pages/layout.py
+++ pages/layout.py
@@ -6,7 +6,7 @@ import streamlit as st
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
-output_string = StringIO()
+
 # with open('samples/simple1.pdf', 'rb') as fin:
 #     extract_text_to_fp(fin, output_string, laparams=LAParams(),
 #                         output_type='html', codec=None)
@@ -16,7 +16,8 @@ st.write(pdfminer.__version__)
 st.write("hshss")
 uploaded_file = st.file_uploader("Choose a file", "pdf")
 if uploaded_file is not None:
-    txt= extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='html', codec=None)
+    output_string = StringIO()
+    txt= extract_text_to_fp(uploaded_file, output_string, laparams=LAParams(), output_type='html', codec=None)
     st.write(txt)
     st.markdown(txt,  unsafe_allow_html=True)
 
